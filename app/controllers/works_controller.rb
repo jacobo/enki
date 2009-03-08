af03857a665c32659b5469dbd82bcb0c6b15eb9a
Jacob Burkhart <igotimac@gmail.com>
2009-03-08 15:42:05 -0400

    category links underscore-ized, homepage 'notes' managed as pages

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,11 +2,11 @@ class WorksController < ApplicationController
   
   def index
     first_category = Work.find(:first, :order => "sort_number ASC").category
-    redirect_to url_for(:category => first_category, :action => 'show')
+    redirect_to url_for(:category => first_category.underscore, :action => 'show')
   end
   
   def show
-    category = params[:category]
+    category = params[:category].humanize
     @works = Work.find(:all, :conditions => ["category = ?", category], :order => "sort_number ASC")
     # @work = Work.find(params[:id])
     # if request.xhr?
