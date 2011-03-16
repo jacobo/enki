8c910a14ee3365d8452b8c092aca41d73ff71220
Jacob Burkhart <jburkhart@engineyard.com>
2011-03-15 21:52:24 -0700

    contact form betterment

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,9 +1,16 @@
 class ContactController < ApplicationController
-  
+
   def index
+    if params[:message]
+      @render_thank_you = true
+    else
+      @render_form = true
+    end
   end
-  
+
   def create
+    Rails.logger.info("TODO: " + params.inspect)
+    redirect_to :action => 'index', :message => "thank_you"
   end
-  
+
 end
