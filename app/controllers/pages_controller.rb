abaaa1fcd92d4851b17e301f1893665710655687
Jacob Burkhart <jburkhart@engineyard.com>
2011-03-28 19:28:53 -0700

    fix homepage overflow

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
     @page = Page.find_by_slug(params[:id]) || raise(ActiveRecord::RecordNotFound)
   end
   def home
+    @home = true
   end
 
   def contact
