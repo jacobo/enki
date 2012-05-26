c835b40a4650a77a09799b54cc7ea1206d4b1497
Jacob Burkhart <jburkhart@engineyard.com>
2012-05-26 14:26:03 -0700

    homepagestuff

--- public/javascripts/birdfolio.js
+++ public/javascripts/birdfolio.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
   $('.works_nav a').pjax();
+  $(".defaultText").focus(function()
+  {
+      if ($(this).val() == $(this)[0].title)
+      {
+          $(this).removeClass("defaultTextActive");
+          $(this).val("");
+      }
+  });
+  $(".defaultText").blur(function()
+  {
+      if ($(this).val() == "")
+      {
+          $(this).addClass("defaultTextActive");
+          $(this).val($(this)[0].title);
+      }
+  });
+  $(".defaultText").blur();
 });
